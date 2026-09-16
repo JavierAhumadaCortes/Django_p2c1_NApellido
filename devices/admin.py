c3e915d74e45522b5c23ca4cdf418240672e871f
Laboratorio Computación N° 7 <claseslab7@inacap.cl>
2026-09-16 12:17:29 -0300

    admin en device y organizations

--- devices/admin.py
+++ devices/admin.py
@@ -1,3 +1,22 @@
+# devices/admin.py
 from django.contrib import admin
+from .models import Device
 
-# Register your models here.
+@admin.register(Device)
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "serial_number",
+        "organization",
+        "zone",
+        "organization__is_active",
+    )
+    search_fields = (
+        "name",
+        "serial_number",
+        "organization__name",
+        "zone__name",
+    )
+    list_filter = ("organization", "zone", "organization__is_active")
+    ordering = ("organization__name", "name")
+    list_select_related = ("organization", "zone")
--- organizations/admin.py
+++ organizations/admin.py
@@ -1,4 +1,44 @@
 from django.contrib import admin
-from .models import Organization
+from .models import Organization, Department, Zone
 
-admin.site.register(Organization)
+#admin.site.register(Organization)
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "tax_id",
+        "is_active",
+        "created_at",
+    )
+    search_fields = ("name", "tax_id")
+    list_filter = ("is_active",)
+    ordering = ("name",)
+    list_per_page = 25
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "organization",
+        "organization__is_active",
+    )
+    search_fields = (
+        "name",
+        "organization__name",
+        "organization__tax_id",
+    )
+    list_filter = ("organization", "organization__is_active")
+    ordering = ("organization__name", "name")
+    list_select_related = ("organization",)
+
+@admin.register(Zone)
+class ZoneAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "organization__name",
+        "created_at",
+    )
+    search_fields = ("name", "organization")
+    list_filter = ("name","organization")
+    ordering = ("name",)
+    list_per_page = 25
